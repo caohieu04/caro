044d57cde5af1a14f8a023b0660b749175a3aea6
chisa <67002310+tailocbmt@users.noreply.github.com>
2020-08-24 11:25:49 +0700

    Add files via upload

--- src/view/View.java
+++ src/view/View.java
@@ -3,11 +3,9 @@
  */
 package view;
 
-import java.io.InputStream;
 import java.net.Socket;
 import java.util.Optional;
 import java.io.*;
-import java.net.*;
 
 import controller.Controller;
 import javafx.application.Platform;
@@ -21,19 +19,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.Labeled;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.BoardState;
 import model.ComputerPlayer;
-import model.Player;
 import model.HumanPlayer;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import gameServer.gameServer;
 
 
 public class View implements EventHandler<ActionEvent> {
@@ -50,16 +45,19 @@ public class View implements EventHandler<ActionEvent> {
 	private Button btnAbout;
 	private Labeled timePlayer1, timePlayer2;
 	private BoardState boardState ;
-	private ComputerPlayer computer ;
+	private ComputerPlayer computer;
+	private HumanPlayer humanPlayer;
+	private boolean connected = false;
+	private gameServer gs;
+
 	//client
 	private int playerID;
 	private int otherPlayer;
 	private ClientSideConnection csc;
-	private int count = 0;
 	private boolean buttonEnable;
 	private String tempstr;
+	private int count;
 	//
-	private HumanPlayer humanPlayer;
 	//
 	// lop dieu khien
 	Controller controller;
@@ -70,39 +68,50 @@ public class View implements EventHandler<ActionEvent> {
 
 	public View() {
 		//
-		ConnectToServer();
 		//
 	}
 	//
-	/*
 	public Object inputname(){
 		JFrame frame = new JFrame();
 		Object result = JOptionPane.showInputDialog(frame, "Enter name:");
 		return result;
 	}
 
-	 */
 	public void start(Stage primaryStage) {
 		try {
 			View.primaryStage = primaryStage;
 			arrayButtonChess = new Button[WIDTH_BOARD][HEIGHT_BOARD];
 			boardState = new BoardState(WIDTH_BOARD, HEIGHT_BOARD);
 			//
-			humanPlayer = new HumanPlayer(boardState);
+			//humanPlayer = new HumanPlayer(boardState);
+			computer = new ComputerPlayer(boardState);
 			//
 			controller = new Controller();
 			controller.setView(this);
-			controller.setPlayer(humanPlayer);
+			controller.setPlayer(computer);
+			//
+			if (controller.getPlayer() instanceof HumanPlayer){
+				ConnectToServer();
+				connected = true;
+				if (playerID == 1){
+					otherPlayer = 2;
+					buttonEnable = false;
+				}
+				else{
+					otherPlayer = 1;
+					buttonEnable = true;
+					updateTurn();
+				}
+			}
 			//
 			BorderPane borderPane = new BorderPane();// tạo cái borderpane
 			BorderPane borderPaneLeft = new BorderPane();
 			BorderPane borderPaneRight = new BorderPane();
 			BorderPane borderPaneTop = new BorderPane();
 			//tạo 2 rectangle để chứa data
-
-			menu(borderPaneRight);
+			menu(borderPaneRight,1);
 			menuGridPane(borderPaneTop);
-			menu(borderPaneLeft);
+			menu(borderPaneLeft,0);
 			//
 			GridPane root = new GridPane();
 			Scene scene = new Scene(borderPane, WIDTH_PANE, HEIGHT_PANE);
@@ -111,7 +120,7 @@ public class View implements EventHandler<ActionEvent> {
 			//
 			borderPane.setTop(borderPaneTop);
 			borderPane.setCenter(root);
-			borderPane.setLeft(borderPaneLeft);//phải xóa dc cái borderright thì cái kia mới vô giữa
+			borderPane.setLeft(borderPaneLeft);
 			borderPane.setRight(borderPaneRight);
 			//set bàn cờ ra giữa
 			BorderPane.setMargin(root,new Insets(10,60,0,220));
@@ -120,22 +129,12 @@ public class View implements EventHandler<ActionEvent> {
 			//
 			// mac dinh player 1 di truoc
 			//
-			if (playerID == 1){
-				otherPlayer = 2;
-				buttonEnable = false;
-
-			}
-			else{
-				otherPlayer = 1;
-				buttonEnable = true;
-				updateTurn();
-			}
-			//
 			controller.setPlayerFlag(1);
 			controller.setTimePlayer(timePlayer1, timePlayer2);
 			for (int i = 0; i < WIDTH_BOARD; i++) {
 				for (int j = 0; j < HEIGHT_BOARD; j++) {
 					Button button = new Button();
+					//button.setStyle("-fx-base: yellow;");
 					//Tạo button
 					button.setPrefSize(43, 43);
 					//set size cho button
@@ -144,18 +143,34 @@ public class View implements EventHandler<ActionEvent> {
 					arrayButtonChess[i][j] = button;
 					//gán button vào bàn cờ, những ô để nhấn
 					root.add(button, j, i);
-					button.setOnAction(new EventHandler<ActionEvent>() {
-						@Override
-						public void handle(ActionEvent event) {
-							if (!controller.isEnd()) {
-								buttonEnable = true;
-								toggleButtons();
-								csc.sendButtonClick(button);
-								updateTurn();
-								controller.play(button.getAccessibleText(), arrayButtonChess);
+					if (connected == true) {
+						int I = i;
+						int J = j;
+						button.setOnAction(new EventHandler<ActionEvent>() {
+							@Override
+							public void handle(ActionEvent event) {
+								if (!controller.isEnd()) {
+									if (boardState.boardArr[I][J] == 0) {
+										buttonEnable = true;
+										toggleButtons();
+										csc.sendButtonClick(button);
+										controller.play(button.getAccessibleText(), arrayButtonChess);
+										updateTurn();
+									}
+								}
 							}
-						}
-					});
+						});
+					}
+					else {
+						button.setOnAction(new EventHandler<ActionEvent>() {
+							@Override
+							public void handle(ActionEvent event) {
+								if (!controller.isEnd()) {
+									controller.play(button.getAccessibleText(), arrayButtonChess);
+								}
+							}
+						});
+					}
 				}
 			}
 			primaryStage.setScene(scene);
@@ -173,10 +188,16 @@ public class View implements EventHandler<ActionEvent> {
 		Thread t = new Thread(new Runnable() {//bug ở đoạn này
 			@Override
 			public void run() {
+				Platform.runLater(()-> {
+					toggleButtons();
+				});
 				tempstr = csc.receiveButton();
 				Platform.runLater(()-> {
 					controller.play(tempstr, arrayButtonChess);
 				});
+				if (controller.isEnd()){
+					csc.closeConnection();
+				}
 				buttonEnable = false;
 				toggleButtons();
 				//send button đến player kia
@@ -197,7 +218,7 @@ public class View implements EventHandler<ActionEvent> {
 		public ClientSideConnection(){
 			System.out.println("----Client----");
 			try {
-				socket = new Socket("localhost",22222);
+				socket = new Socket("localhost",60000);
 				dataIn = new DataInputStream(socket.getInputStream());
 				dataOut = new DataOutputStream(socket.getOutputStream());
 				playerID = dataIn.readInt();//lấy player id từ server
@@ -227,12 +248,24 @@ public class View implements EventHandler<ActionEvent> {
 			}
 			return btnContent;
 		}
+		//close connection
+		public void closeConnection(){
+			try {
+				socket.close();
+				System.out.println("Connection close");
+			}
+			catch (IOException ex){
+				System.out.println("IOException from closeConnection CSC");
+			}
+		}
 	}
 	//method để disable button
 	public void toggleButtons() {
 		for (int i = 0;i<WIDTH_BOARD; i++) {
 			for (int j=0;j<HEIGHT_BOARD;j++){
 				arrayButtonChess[i][j].setDisable(buttonEnable);
+				//method giu mau button khi disable
+				arrayButtonChess[i][j].setOpacity(1);
 			}
 		}
 	}
@@ -243,24 +276,7 @@ public class View implements EventHandler<ActionEvent> {
 		VBox box = new VBox();
 		box.setSpacing(10);
 		Class<?> clazz = this.getClass();
-		//AnchorPane anchorPaneLogo = new AnchorPane();
-		//AnchorPane gridPane = new AnchorPane();
 		GridPane gridPane = new GridPane();
-
-		// set logo
-		/*InputStream input = clazz.getResourceAsStream("/image/Logo.jpg");
-		Image image = new Image(input);
-		ImageView imgView = new ImageView(image);
-		imgView.setFitHeight(230);
-		imgView.setFitWidth(260);
-		AnchorPane.setTopAnchor(imgView, 10.0);
-		AnchorPane.setLeftAnchor(imgView, 30.0);
-		AnchorPane.setRightAnchor(imgView, 30.0);
-		anchorPaneLogo.add(imgView);
-
-
-		 */
-
 		// Computer
 		btnComputer = new Button("Chơi với máy");
 		btnComputer.setId("btnMenu");
@@ -308,89 +324,31 @@ public class View implements EventHandler<ActionEvent> {
 		box.getChildren().add(gridPane);
 		pane.setLeft(box);
 	}
-	private void menu(BorderPane pane) {
+	private void menu(BorderPane pane,int n) {
 		VBox box = new VBox();
 		box.setSpacing(10);
 		Class<?> clazz = this.getClass();
-		//AnchorPane anchorPaneLogo = new AnchorPane();
-		//AnchorPane gridPane = new AnchorPane();
+
 		GridPane gridPane = new GridPane();
-
-		// set logo
-		/*InputStream input = clazz.getResourceAsStream("/image/Logo.jpg");
-		Image image = new Image(input);
-		ImageView imgView = new ImageView(image);
-		imgView.setFitHeight(230);
-		imgView.setFitWidth(260);
-		AnchorPane.setTopAnchor(imgView, 10.0);
-		AnchorPane.setLeftAnchor(imgView, 30.0);
-		AnchorPane.setRightAnchor(imgView, 30.0);
-		anchorPaneLogo.add(imgView);
-
-
-		 */
-
-		// Computer
-		/*
-		btnComputer = new Button("Chơi với máy");
-		btnComputer.setId("btnMenu");
-		btnComputer.setOnAction(this);
-		
-		gridPane.add(btnComputer,0,0);
-		// Human
-		btnHuman= new Button("Hai người chơi");
-		btnHuman.setId("btnMenu");
-		btnHuman.setOnAction(this);
-		
-		gridPane.add(btnHuman,1,0);
-		
-		// Undo
-		btnUndo = new Button("Quay lại");
-		btnUndo.setId("btnMenu");
-		btnUndo.setOnAction(this);
-		
-		gridPane.add(btnUndo,2,0);
-		// Save
-		btnSave = new Button("Lưu lại");
-		btnSave.setId("btnMenu");
-		btnSave.setOnAction(this);
-	
-		gridPane.add(btnSave,3,0);
-		// Load
-		btnLoad = new Button("Load lại");
-		btnLoad.setId("btnMenu");
-		btnLoad.setOnAction(this);
-		
-		gridPane.add(btnLoad,4,0);
-		// About
-		btnAbout = new Button("Thông tin");
-		btnAbout.setId("btnMenu");
-		btnAbout.setOnAction(this);
-		
-		gridPane.add(btnAbout,5,0);
-		// exit
-		btnExit = new Button("Thoát");
-		btnExit.setId("btnMenu");
-		btnExit.setOnAction(this);
-	
-		gridPane.add(btnExit,6,0);
-		//
-		box.getChildren().add(gridPane);
-
-
-
-		 */
-
-
-
-		// Bottom
 
 		GridPane gridPaneBottom = new GridPane();
 		//
 		Labeled namePlayer;
+		if (n == 0){
+			timePlayer1 = new Label("15");
+			timePlayer1.setId("timeplayer");
 
-		namePlayer = new Label("Player "+playerID);
+			namePlayer = new Label("Player " + 1);
+			gridPaneBottom.add(timePlayer1, 0, 1);
+		}
+		else {
+			timePlayer2 = new Label("15");
+			timePlayer2.setId("timeplayer");
 
+			namePlayer = new Label("Player " + 2);
+			gridPaneBottom.add(timePlayer2, 0, 1);
+
+		}
 		//
 		namePlayer.setId("nameplayer");//để lấy màu chữ
 		//Labeled namePlayer2 = new Label("Player 2");
@@ -398,20 +356,12 @@ public class View implements EventHandler<ActionEvent> {
 		gridPaneBottom.add(namePlayer, 0, 0);
 		//gridPaneBottom.add(namePlayer2, 1, 0);
 		//box.getChildren().add(gridPaneBottom);
-		//
-
-
-		timePlayer1 = new Label("60");
-		timePlayer1.setId("timeplayer");
-		timePlayer2 = new Label("60");
-		timePlayer2.setId("timeplayer");
-		gridPaneBottom.add(timePlayer1, 0, 1);
 		box.getChildren().add(gridPaneBottom);
 		//
 		pane.setCenter(box);
 	}
 
-	
+
 	@Override
 	public void handle(ActionEvent e) {
 		if (e.getSource() == btnExit) {
@@ -440,16 +390,18 @@ public class View implements EventHandler<ActionEvent> {
 
 	// che do dau voi may
 	public void replayComputer() {
-		
+
 		controller.setEnd(false);
 		controller.setTimePlayer(timePlayer1, timePlayer2);
 		controller.setPlayer(new ComputerPlayer(new BoardState(WIDTH_BOARD, HEIGHT_BOARD)));
 		controller.reset(arrayButtonChess);
 		gameMode();
-		
+
 	}
 	// che do 2 nguoi choi
 	public void replayHuman() {
+		gs = new gameServer();
+		gs.acceptConnection();
 		controller.setEnd(false);
 		controller.setTimePlayer(timePlayer1, timePlayer2);
 		controller.setPlayer(new HumanPlayer(new BoardState(WIDTH_BOARD, HEIGHT_BOARD)));
@@ -461,8 +413,8 @@ public class View implements EventHandler<ActionEvent> {
 	public void aboutUs() {
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("About us");
-		alert.setHeaderText("");
-		alert.setContentText("");
+		alert.setHeaderText("Credit");
+		alert.setContentText("NTL");
 		alert.showAndWait();
 	}
 	// xet xem ai di truoc
